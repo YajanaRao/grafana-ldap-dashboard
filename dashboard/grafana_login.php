c3e09a207226cbcb3628bd6307e6cd81fb96236d
Yajana N Rao <38914337+YajanaRao@users.noreply.github.com>
2018-08-13 13:01:45 +0530

    Update grafana_login.php

--- dashboard/grafana_login.php
+++ dashboard/grafana_login.php
@@ -1,17 +1,5 @@
 
 <?php
-
-    
-    # https://stackoverflow.com/questions/830074/can-a-username-and-password-be-sent-safely-over-https-via-url-parameters
-    // $username = "anthony";
-    // $password = "password";
-    // $credencials = $username.':'.$password;
-    // echo $credencials;
-    // $cred = base64_encode($credencials);
-    // echo $cred;
-    // $url = "http://$credencials@192.168.99.100/";
-    // echo $url;
-    // header( "Location: $url" ) ;
     session_start(); 
     if (isset($_SESSION['username']) && isset($_SESSION['password'])) 
     {
@@ -22,10 +10,6 @@
         $credencials = $username.':'.$password;
         $cred = base64_encode($credencials);
         $url = "http://$credencials@localhost/grafana/";
-        // echo $cred;
-        // echo $url;
-        // $url = "http://localhost/grafana/";
-        // header("Authorization: Basic ".$cred);
         header("Location: $url");
     }
 
