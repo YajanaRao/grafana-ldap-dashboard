f660ad4a30b793975f8037d1a39d75348cad5df2
Yajana N Rao <38914337+YajanaRao@users.noreply.github.com>
2018-08-23 12:21:28 +0530

    Update ldap.php

--- dashboard/config/ldap.php
+++ dashboard/config/ldap.php
@@ -1,1 +1,20 @@
 
+
+<?php
+/**
+ * 
+ */
+class Ldap 
+{
+	
+	function getConnection()
+	{
+		# code...
+		$this->ldap = ldap_connect("ldap://localhost:389");  // assuming the LDAP server is on this host
+		ldap_set_option($this->ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+		ldap_set_option($this->ldap, LDAP_OPT_REFERRALS, 0);
+		return $this->ldap;
+	}
+}
+
+?>
